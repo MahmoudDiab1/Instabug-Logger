ea8f931ecb8fce6937d9a3a73c2b9de81ca84ae6
Mahmoud Diab <mdiab@istech.com>
2021-05-29 02:32:26 +0200

    dep: missed files deployed

---
+++ InstabugInternshipTask/RootViewController.swift 00-48-25-350.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  InstabugInternshipTask
+//
+//  Created by Yosef Hamza on 19/04/2021.
+//
+
+import UIKit
+import InstabugLogger
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
---
+++ InstabugInternshipTask/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  InstabugInternshipTask
+//
+//  Created by mahmoud diab on 29/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
